ef7d05a5f481e13fa45264ddfb6dfab3e7df8ca5
Wang Shijiang <403036847@qq.com>
2018-01-20 12:20:22 +0800

    课程信息类使用Eg1

--- Shnu-course-analyze.py
+++ Shnu-course-analyze.py
@@ -1,11 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
 import Shnu_course
 
 course_helper = Shnu_course.CourseHelper()
 
 for course in course_helper.get_page_courses(60):
     print '####'
-    print course.course_id
-    print course.course_name
-    print course.course_teacher
+    print course.name
+
+    print course.places[0].week
+    print course.places[0].time
+    print course.places[0].week_duration
+    print course.places[0].teacher
+    print course.places[0].place
+
+
     print '####'
 
