8b2ec03ddfaae10e4de4956de27485c774c2c12e
Wang Shijiang <403036847@qq.com>
2018-02-18 20:46:42 +0800

    删除了导入数据的傻逼在班级后面加的

---
+++ Data_filiter.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import os
+
+folder = 'semester201702'
+
+i = 1
+
+while os.path.exists('data/{0}/course_page{1}.txt'.format(folder, i)):
+    print 'data/{0}/course_page{1}.txt'.format(folder, i)
+
+    with open('data/{0}/course_page{1}.txt'.format(folder, i), 'r') as my_file_raw:
+        lines = my_file_raw.readlines()
+
+    with open('data/{0}/course_page{1}.txt'.format(folder, i), 'w') as my_file:
+        for line in lines:
+            if '&' in line:
+                line = line.replace('&', '')
+            my_file.write(line)
+
+    i += 1
